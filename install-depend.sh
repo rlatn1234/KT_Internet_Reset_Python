70233014b7dfd360a4bf5897438598b6f2890680
Kim Su <qnrwhr01@gmail.com>
2021-04-19 14:02:23 +0900

    Create install-depend.sh

---
+++ install-depend.sh
@@ -0,0 +1,5 @@
+apt-get install -y python3 python3-pip python3-dev build-essential git libxss1 libgconf2-4 libappindicator1 libindicator7
+git clone https://github.com/rlatn1234/KT_Internet_Reset_Python
+cd KT_Internet_Reset_Python
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
