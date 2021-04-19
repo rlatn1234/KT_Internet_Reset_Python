04c8ab13ef12a2193a4973a4227c7fa555a4f35d
Kim Su <qnrwhr01@gmail.com>
2021-04-19 14:31:36 +0900

    Update install-depend.sh

--- install-depend.sh
+++ install-depend.sh
@@ -1,5 +1,6 @@
 apt-get install -y python3 python3-pip python3-dev build-essential git libxss1 libgconf2-4 libappindicator1 libindicator7
+apt-get install -yqq unzip curl
 git clone https://github.com/rlatn1234/KT_Internet_Reset_Python
 cd KT_Internet_Reset_Python
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
